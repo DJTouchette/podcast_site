f4656ce884b4a41c7f5dae57432b66d3174ae8c3
Damien <djtouchette1993@gmail.com>
2016-03-09 13:32:05 -0800

    added ajax requests to itunes for top 10 and search

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,33 @@
-$(document).ready(function() {
+$(function() {
+  button = $('#btn');
+  button.on('click', function () {
+    console.log('btn clicked');
+    $('#contacts').empty();
+    //top 10
+    $.ajax({
+      url:'https://itunes.apple.com/ca/rss/toppodcasts/limit=10/explicit=true/json',
+      dataType: 'json',
+      success: function(data){
+        $('#btn').css('color', 'red');
+        console.log(data);
+        }
+      });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+      //Search
+    $.ajax({
+      url:'https://itunes.apple.com/search?term=joe&country=ca&entity=podcast',
+      dataType: 'jsonp',
+      success: function(data){
+        $('#btn').css('color', 'blue');
+        console.log(data);
+        }
+      });
+    });
+
+    function getInfo() {
+
+    }
+
+
+
 });
