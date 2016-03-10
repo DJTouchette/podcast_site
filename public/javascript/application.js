4ea3a82edd6933a1ed8fafb89802579c9470c05d
Damien <djtouchette1993@gmail.com>
2016-03-09 23:06:54 -0800

    Added search, changed UX

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,65 +1,25 @@
 $(function() {
 
-// Top 10 screen
+  $.ajax({
+    url:'https://itunes.apple.com/us/rss/toppodcasts/limit=100/json',
+    dataType: 'json',
+    success: function(data){
+      display(data);
+      }
+    });
 
   var btn = $('#btn');
 
   btn.on('click', function () {
-
+    $('#main-screen').empty();
     $.ajax({
-      url:'https://itunes.apple.com/ca/rss/toppodcasts/limit=10/explicit=true/json',
+      url:'https://itunes.apple.com/ca/rss/toppodcasts/limit=12/explicit=true/json',
       dataType: 'json',
       success: function(data){
-        getInfo(data);
+      display(data);
         }
       });
-
-      //Search
-    $.ajax({
-      url:'https://itunes.apple.com/search?term=joe&country=ca&entity=podcast',
-      dataType: 'jsonp',
-      success: function(data){
-        // console.log(data);
-        }
-      });
-    });
-
-    function getInfo(data) {
-      $('#main-screen').empty();
-      var dfe = data.feed.entry;
-
-      for (var i = 0; i < data.feed.entry.length; i++ ) {
-
-        var divCol8 = $('<div>').attr('class', 'col-xs-10');
-        var divCol4 = $('<div>').attr('class', 'col-xs-2');
-        var divRow = $('<div>').attr('class', 'row');
-        var img = $('<img>').attr('class', 'podimg');
-        var artist = $('<h4>');
-        var title = $('<h2>');
-        var id = $('<data-id>');
-
-
-        title.text( dfe[i]['im:name'].label );
-        artist.text( dfe[i]['im:artist'].label );
-        img.attr( 'src', dfe[i]['im:image'][1].label );
-        img.attr( 'data-id', dfe[i].id.attributes['im:id']);
-        console.log(dfe[i].id.attributes['im:id']);
-
-        $(divRow).appendTo('#main-screen');
-        $(divCol4).appendTo(divRow);
-        $(img).appendTo(divCol4);
-        $(divCol8).appendTo(divRow);
-        $(title).appendTo(divCol8);
-        $(artist).appendTo(divCol8);
-
-      }
-      $( ".podimg" ).on('click', function () {
-        var id = $(this).attr('data-id');
-        getEpisode(id);
-      });
-       $( "#main-screen" ).slideToggle(800);
-    }
-
+      
     function getEpisode(id) {
       var url = ['https://itunes.apple.com/lookup?id=', id].join('');
 
@@ -75,12 +35,7 @@ $(function() {
     function episodeScreen(data) {
       var dr = data.results[0];
       $('#main-screen').empty();
-      // var artist = dr.artistName;
-      // var img = dr.artworkUrl600;
-      // var itunes = dr.collectionViewUrl;
-      // var genre = dr.primaryGenreName;
-      // var title = dr.collectionCensoredName;
-      console.log("i made it");
+
       var divCol8 = $('<div>').attr('class', 'col-xs-6');
       var divCol4 = $('<div>').attr('class', 'col-xs-6');
       var divRow = $('<div>').attr('class', 'row');
@@ -97,7 +52,6 @@ $(function() {
       img.attr( 'src', dr.artworkUrl600 );
       genre.text(dr.primaryGenreName);
 
-
       $(divRow).appendTo('#main-screen');
       $(divCol4).appendTo(divRow);
       $(divCol4).prepend(img);
@@ -109,6 +63,51 @@ $(function() {
 
 
     }
+
+    $('#search').on('click', function(event){
+      event.preventDefault();
+      searchTerm = $('#search-term').val().replace(' ', '+');
+      var url = ['https://itunes.apple.com/search?term=', searchTerm,'&country=ca&entity=podcast'].join('');
+      $(this).closest('form').find("input[type=text], textarea").val("");
+      $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        success: function(data){
+          episodeScreen(data);
+          }
+        });
+      });
+
+       function display(data) {
+          var dfe = data.feed.entry;
+            for (var i = 0; i < data.feed.entry.length; i++ ) {
+
+              var divCol4 = $('<div>').attr('class', 'col-xs-4');
+              var img = $('<img>').attr('class', 'podimg');
+              var id = $('<data-id>');
+
+              var url = dfe[i]['im:image'][1].label.slice(0, (dfe[i]['im:image'][1].label.length - 14));
+              var urlDone = [url, '500x500.jpg'].join('');
+
+              img.attr( 'src', urlDone );
+              img.attr( 'data-id', dfe[i].id.attributes['im:id']);
+              console.log(dfe[i].id.attributes['im:id']);
+
+              $(divCol4).appendTo('#main-screen');
+              $(img).appendTo(divCol4);
+            }
+            $( ".podimg" ).on('click', function () {
+              var id = $(this).attr('data-id');
+              getEpisode(id);
+            });
+
+
+      }
+
+
+
+
+
 
 
 });
